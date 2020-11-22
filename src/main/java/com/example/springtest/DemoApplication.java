31353a7ea08fe4394016b703ce2205aca4a6ea93
Mohomed Nazar Nazoor Ahamed <nazar.2016355@iit.ac.lk>
2020-11-23 02:36:55 +0530

    4 commit

--- src/main/java/com/example/springtest/DemoApplication.java
+++ src/main/java/com/example/springtest/DemoApplication.java
@@ -3,8 +3,14 @@ package com.example.springtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -17,8 +23,13 @@ public class DemoApplication {
 @RestController
 class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("/hello")
 	String home() {
 		return "Hello World!";
+	}
+
+	@GetMapping("/genres")
+	public ResponseEntity<Set<String>> findAllGenres() {
+		return new ResponseEntity<>(new TreeSet<>(Arrays.asList("Technology", "Science", "History", "Fantasy", "Biography", "Horror", "Romance")), HttpStatus.OK);
 	}
 }
